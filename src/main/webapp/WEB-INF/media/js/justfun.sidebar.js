5e7c17960f471872854e57391b163e305c9f8ee6
liupeng <124946122@qq.com>
2016-08-18 11:09:57 +0800

    集成easyui

---
+++ src/main/webapp/WEB-INF/media/js/justfun.sidebar.js
@@ -0,0 +1,57 @@
+(function($) {
+  $.fn.jfSidebar = function(options) {
+    var opts = $.extend({}, $.fn.jfSidebar.defaults, options);
+    return this.each(function() {
+      $this = $(this);
+      $this.find('li > a').each(function(i,item) {
+        var a = $(this);
+        var idx = i + 1;
+        a.data("tabIndex", idx);
+        a.attr("id", "a_" + idx);
+        a.click(function () {
+            active(a, false);
+            return false;
+        }).dblclick(function() {
+            active(a, true);
+            return false;
+        });
+      });
+    });
+  };
+  function active(a,opts) {
+    if (!a.next().hasClass('sub-menu')) {
+      clickMenu(a,opts);
+      return false;
+    }
+    var parent = a.parent().parent();
+    parent.children('li.open').children('a').children('.arrow').removeClass('open');
+    parent.children('li.open').children('.sub-menu').slideUp(200);
+    parent.children('li.open').removeClass('open');
+    var sub = a.next();
+    if (sub.is(":visible")) {
+      jQuery('.arrow', a).removeClass("open");
+      a.parent().removeClass("open");
+      sub.slideUp(200, function () {});
+    } else {
+      jQuery('.arrow', a).addClass("open");
+      a.parent().addClass("open");
+      sub.slideDown(200, function () {});
+    }
+  };
+  function clickMenu(a,opts) {
+    var doClick = opts.clickEvent;
+    if (!doClick) {
+      return false;
+    }
+    doClick(a);
+  };
+  $.fn.jfSidebar.defaults = {
+    clickEvent: function(a) {
+      if (a) {
+        alert(a.title);
+      } else {
+        alert("no title");
+      }
+    }
+  };
+})(jQuery);
